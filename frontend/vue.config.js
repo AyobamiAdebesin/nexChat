c2d96a1af18af9f53f50395436872c3e5d260dd7
AyobamiAdebesin <froshofficial@gmail.com>
2023-03-12 23:20:06 +0100

    configured proxying from the backend

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,3 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+// vue.config.js
+    
+module.exports = {
+  devServer: {
+    proxy: {
+      "/backend/api/v1": {
+        target: "http://localhost:5000",
+        ws: false,
+        changeOrigin: true
+      }
+    }
+  }
+};
