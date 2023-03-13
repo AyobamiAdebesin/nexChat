9bdcdacd277ef3720f7955f567221d89dbae2ca0
AyobamiAdebesin <froshofficial@gmail.com>
2023-03-13 23:16:20 +0100

    added init

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ Endpoint for abstracted database """
 import os
-from models.base_model import BaseModel, Base
-from models.channels import Channel
-from models.messages import Message
-from models.users import User
-from models.engine.db_storage import DBStorage
+from backend.models.base_model import BaseModel, Base
+from backend.models.channels import Channel
+from backend.models.messages import Message
+from backend.models.users import User
+from backend.models.engine.db_storage import DBStorage
 
 storage = DBStorage()
 storage.reload()
