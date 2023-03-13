f2f5183fe24145c289c4656c8e16c638c80fdd4f
AyobamiAdebesin <froshofficial@gmail.com>
2023-03-13 11:02:28 +0100

    fixing relative import error

--- backend/api/v1/app.py
+++ backend/api/v1/app.py
@@ -2,23 +2,38 @@
 """ Entry point for backend """
 import flask
 import os
-from flask import Flask, request, jsonify
-import dotenv
+#import dotenv
 import pusher
-from models import storage
-
+from flask import Flask, request, jsonify
+from models.engine.db_storage import DBStorage
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     """ Index route """
     return jsonify('Hello World')
 
-@app.route('/home')
-def home():
-    """ Return home page """
-    return jsonify({'Hello': 'World'})
+
+@app.route('/api/v1/register', methods=['POST'])
+def register():
+    """ Register route """
+    data = request.get_json()
+    username = data['username']
+    password = generate_password_hash(data['password'])
+    
+    try:
+        user = User(username=username, password=password)
+        storage.new(user)
+        storage.save()
+    except:
+        return jsonify({'error': 'User already exists'}), 400
+    return jsonify({'success': 'User created'}), 201
+
+@app.route('/login')
+
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
