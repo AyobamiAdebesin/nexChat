6cee9d6633216b1a664d59ee7c57a7a5f06944d1
AyobamiAdebesin <froshofficial@gmail.com>
2023-03-13 23:12:01 +0100

    Deleted file

--- backend/setup_twibbly_db.sql
+++
@@ -1,5 +0,0 @@
--- setup development database in MySQL server
-CREATE DATABASE IF NOT EXISTS twibbly_dev_db;
-CREATE USER IF NOT EXISTS 'twibbly_dev'@'localhost' IDENTIFIED BY 'Olaseni..1996';
-GRANT ALL PRIVILEGES ON twibbly_dev_db.* TO 'twibbly_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'twibbly_dev'@'localhost';
