5aed36ffd9770164225f2e5b4cb41a7f216b654e
AyobamiAdebesin <froshofficial@gmail.com>
2023-03-14 01:19:44 +0100

    Creatd request_chat endpoint

--- backend/api/v1/app.py
+++ backend/api/v1/app.py
@@ -9,7 +9,7 @@ from flask import Flask, request, jsonify
 from backend.models.users import User
 from sqlalchemy.orm.exc import NoResultFound
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import JWTManager 
+from flask_jwt_extended import JWTManager
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
@@ -18,6 +18,14 @@ from flask_jwt_extended import get_jwt_identity
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = os.getenv('TWIBBLY_JWT_SECRET')
 jwt = JWTManager(app)
+
+# Pusher setup
+pusher = pusher.Pusher(
+    app_id=os.getenv('TWIBBLY_PUSHER_APP_ID'),
+    key=os.getenv('TWIBBLY_PUSHER_KEY'),
+    secret=os.getenv('TWIBBLY_PUSHER_SECRET'),
+    cluster=os.getenv('TWIBBLY_PUSHER_CLUSTER'),
+    ssl=True)
 
 
 @app.route('/')
@@ -60,8 +68,10 @@ def login():
         else:
             return jsonify({'error': 'Invalid password'}), 401
 
-#Protect a route with jwt_required, which will kick out requests
-#without a valid JWT present.
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+
+
 @app.route("/api/v1/protected", methods=["GET"])
 @jwt_required()
 def protected():
@@ -69,7 +79,46 @@ def protected():
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
 
- 
+
+# Generate channel
+@app.route('/api/v1/request_chat', methods=['POST'], strict_slashes=False)
+@jwt_required()
+def request_chat():
+    """ Generates a channel for two users """
+    from_user = request.form.get('from_user', '')
+    to_user = request.form.get('to_user', '')
+    to_user_channel = f"private-notification_user_{to_user}"
+    from_user_channel = f"private-notification_user_{from_user}"
+
+    # Check if a channel already exists between the two users
+    try:
+        channel = storage.find_channel_by(from_user=from_user, to_user=to_user)
+    except NoResultFound:
+        channel_name = f"private-chat_{from_user}_{to_user}"
+        new_channel = Channel()
+        new_channel.from_user = from_user
+        new_channel.to_user = to_user
+        new_channel.name = channel_name
+        new_channel.save()
+    else:
+        channel_name = channel.name
+        data = {
+            "from_user": from_user,
+            "to_user": to_user,
+            "from_user_notification_channel": from_user_channel,
+            "to_user_notification_channel": to_user_channel,
+            "channel_name": channel_name
+        }
+
+    # Trigger an event on the to_user's notification channel
+    pusher.trigger(to_user_channel, 'new_chat', data)
+    return jsonify({
+        'status': 'success',
+        'message': 'Channel created',
+        'data': data
+    }), 201
+
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """ Closes the storage on teardown """
